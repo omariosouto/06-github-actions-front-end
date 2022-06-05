3d437f754e3c797d9a0405c39545b7b528ed9f3e
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-05 16:26:25 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import NextLink from "next/link";
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Página Inicial</h1>
+      <h1>Página Inicial [nova alteração!]</h1>
       <NextLink href="/sobre">
         <a>Ir para a sobre</a>
       </NextLink>
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,8 +3,11 @@ import { execSync } from "child_process";
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf8" });
-console.log(output);
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.lenth - 1];
 console.log("[DEPLOY_PREVIEW]: END");
+
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 // console.log("[GITHUB_COMMENT]: START");
 // console.log("[GITHUB_COMMENT]: END");
