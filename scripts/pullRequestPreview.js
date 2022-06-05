3711559f6362e69f03866776eb1b6d2bbe2279da
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-05 16:21:54 -0300

    UPDATE novo

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,10 @@
-console.log("Olá mundo!");
+import { execSync } from "child_process";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
+
+// console.log("[GITHUB_COMMENT]: START");
+// console.log("[GITHUB_COMMENT]: END");
