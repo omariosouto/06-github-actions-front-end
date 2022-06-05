67d8803e8033c6e6422b272f4d6acff4e81bf376
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-05 16:04:34 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import NextLink from "next/link";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>Página Inicial</h1>
+      <NextLink href="/sobre">
+        <a>Ir para a sobre</a>
+      </NextLink>
     </div>
   );
 }
