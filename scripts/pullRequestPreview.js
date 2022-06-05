22b56537eff365bfb9d02507b177addf483b07a6
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-06-05 16:28:06 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -4,7 +4,7 @@ console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf8" });
 const outputLines = output.split("\n");
-const DEPLOY_URL = outputLines[outputLines.lenth - 1];
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
 
 console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
