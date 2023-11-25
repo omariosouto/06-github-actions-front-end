15dc666b2991dabc19e8851ae1bc1d2b1984f32e
Eduardo <eduardo.hoths@gmail.com>
2023-11-25 00:28:58 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd5a1db11152b8c0"; }
+/******/ 		__webpack_require__.h = function() { return "4d44535264c37156"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 export const AboutScreen = () => {
-return (
-<div>
-<h1>sobre</h1>
-</div>
-);
+  return (
+    <div>
+      <h1>sobre</h1>
+    </div>
+  );
 };
-
